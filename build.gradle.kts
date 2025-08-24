cbcc2edda60cf40f723d8863d67d01c2cef70023
AG-techy <aghosh8415@gmail.com>
2025-08-24 14:34:13 +0100

    Update1 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
+    // Android Gradle Plugin
     id("com.android.application") version "8.5.2" apply false
+    // Kotlin Android
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    // Kotlinx Serialization (needed for JSON, etc.)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
 }
